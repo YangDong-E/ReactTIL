18a7b7ddaba5e5b9096150dbcb2520f7c0d3a160
Dong Oh Kang <ehddh125@gmail.com>
2023-10-26 23:10:33 +0900

    react-blog

---
+++ blog/blog-backend/src/index.js
@@ -0,0 +1,51 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+    ctx.body = '홈';
+});
+
+router.get('/about/:name?', (ctx) => {
+    const { name } = ctx.params;
+    // name의 존재 유무에 따라 다른 결과 출력
+    ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+    const { id } = ctx.query;
+    // id의 존재 유무에 따라 다른 결과 출력
+    ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+    console.log('Listening to port 4000');
+});
+// app.use(async (ctx, next) => {
+//     console.log(ctx.url);
+//     console.log(1);
+//     if (ctx.query.authorized !== '1') {
+//         ctx.status = 401; // Unauthorized
+//         return;
+//     }
+//     await next();
+//     console.log('END'); // next를 주석처리를 하면 다음 미들웨어를 무시한다.
+// });
+
+// app.use((ctx, next) => {
+//     console.log(2);
+//     next();
+// });
+
+// // app.use - 미들웨어 함수를 애플리케이션에 등록
+// // 첫번째 파라미터 - ctx(Context) 웹 요청과 응답에 관한 정보를 지닌다.
+// // 두번째 파라미터 - next 함수를 호출하지않으면 다음 미들웨어를 처리하지않음.
+// app.use((ctx) => {
+//     ctx.body = 'hello world';
+// });
