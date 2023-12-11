73ffa7d5181e29e63f699e4da2a8c0d2e08de1d7
Dong Oh Kang <ehddh125@gmail.com>
2023-12-12 00:59:21 +0900

    23.12.12 - 글쓰기 API 연동

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Responsive from '../components/common/Responsive';
 // import Editor from '../components/write/Editor';
 // import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
+// import WriteActionButtons from '../components/write/WriteActionButtons';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
       <TagBoxContainer />
-      <WriteActionButtons />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
