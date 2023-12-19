91eb274db494d4b57ff74c263c9b22cce0c57a6e
Dong Oh Kang <ehddh125@gmail.com>
2023-12-19 21:31:40 +0900

    23.12.19 - 페이지네이션 구현

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 // import PostList from '../components/posts/PostList';
 
 const PostListPage = () => {
@@ -8,6 +9,7 @@ const PostListPage = () => {
     <>
       <HeaderContainer />
       <PostListContainer />
+      <PaginationContainer />
     </>
   );
 };
