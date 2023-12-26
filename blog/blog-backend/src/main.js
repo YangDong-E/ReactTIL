5a61b0be9b1439cad2c7550906014b84076af0a2
Dong Oh Kang <ehddh125@gmail.com>
2023-12-26 19:06:37 +0900

    23.12.26 - koa-static으로 정적파일 제공(프로젝트 마무리)

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -8,6 +8,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import createFakeData from './createFakeData';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -59,6 +62,16 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+    // Not Found 이고, 주소가 /api 로 시작하지 않는 경우
+    if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+        // index.html 내용을 반환
+        await send(ctx, 'index.html', { root: buildDirectory });
+    }
+});
 
 const port = PORT || 4000;
 
