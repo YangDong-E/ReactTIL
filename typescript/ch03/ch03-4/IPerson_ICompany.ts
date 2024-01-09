42c05c0baa275b5643b2e9e585f4d602c0d62d57
Dong Oh Kang <ehddh125@gmail.com>
2024-01-09 19:58:51 +0900

    24.01.09 - 객체의 비구조화 할당문

--- typescript/ch03/ch03-4/IPerson_ICompany.ts
+++ typescript/ch03/ch03-4/IPerson_ICompany.ts
@@ -1,3 +1,11 @@
+// 구조화가 필요한 코드 예시
+let personName = 'Jack';
+let personAge = 32;
+
+let companyName = 'Apple Company, Inc';
+let companyAge = 43;
+
+// 위 코드를 구조화한 코드
 export interface IPerson {
     name: string;
     age: number;
--- typescript/ch03/ch03-4/destructuring.ts
+++ typescript/ch03/ch03-4/destructuring.ts
@@ -1,5 +1,8 @@
 import { IPerson } from './IPerson_ICompany';
 
+// 비구조화
 let jack: IPerson = { name: 'Jack', age: 32 };
+
+// 비구조화 할당
 let { name, age } = jack;
 console.log(name, age); // Jack 32
